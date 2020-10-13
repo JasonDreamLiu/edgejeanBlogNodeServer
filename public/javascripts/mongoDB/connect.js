68adbbe1bf50afd22f54afb2f5a2e0ff8ec2e26c
Yumeng <xaiweiok@vip.qq.com>
2020-10-14 00:52:58 +0800

    更新

--- public/javascripts/mongoDB/connect.js
+++ public/javascripts/mongoDB/connect.js
@@ -1,23 +1,9 @@
-var MongoClient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient;
+
 // var url = "mongodb://edgejeanblog:sdd19961103@118.25.150.243/edgejeanblog";
 
-function connect(user,pws,host,post,database){
-    this.url = `mongodb://${user}:${pws}@${host}${post?":"+post:""}/${database}`;
-    const dbName = database?database:user;
-    this.connect = (fc,obj)=>{
-        MongoClient.connect(this.url, { useNewUrlParser:true }, (err, db) => {
-            if (err) throw err;
-            console.log("数据库已创建！");
-            if (typeof fc === "function"){
-                fc(obj,db,dbName);
-            }else {
-                db.close();
-            }
-        });
-    }
-}
 
-export default function mongoDB_Obj(url){
+function mongoDB_Obj(url){
     this.user = url.user;
     this.pws = url.pws;
     this.host = url.host;
@@ -36,21 +22,86 @@ export default function mongoDB_Obj(url){
     }else if (!this.database){
         this.database = "";
     }
-    let connect = new connect(this.user,this.pws,this.host,this.post,this.database);
+    const connect = new connect_(this.user,this.pws,this.host,this.post,this.database);
 
     this.collection = (name) => {
-        connect.connect(collection,{name});
+        let a = connect.connect(collection,{name});
     }
-}
+    this.insert = (obj,collectionName) => {
+        let a = connect.connect(insert,{obj,collectionName});
+    }
+    this.find = (obj,collectionName) => {
+        return connect.connect(find,{obj,collectionName});
+    }
 
-function collection(obj,db,dbName){
+    function connect_(user,pws,host,post,database){
+        this.url = `mongodb://${user}:${pws}@${host}${post?":"+post:""}/${database}`;
+        const dbName = database?database:user;
+        this.connect = async (fc,obj,res)=>{
+            let conn = null;
+            let a = null;
+            try{
+                conn = await MongoClient.connect(this.url);
+                console.log("数据库已连接！");
+                const dbase = conn.db(dbName);
+                a = fc(obj, dbase, res);
+            } catch (err){
+                console.error(`错误: ${err}`);
+            } finally {
+                if (conn != null) conn.close();
+                return a;
+            }
+        }
+    }
+
+}
+function collection(obj,dbase){
     const {name} = obj;
-    if (!(typeof name === "string" && name))
+    if (!(typeof name === "string" && name)){
+        console.log('请输入String类型的集合名');
         return;
-    const dbase = db.db(dbName);
-    dbase.createCollection(name,(err,res)=>{
-        if (err) throw err;
-        console.log(`创建集合：${name}`);
-        db.close();
+    }
+
+    dbase.createCollection(name).then(result=>{
+        console.log(`创建集合: ${name} 成功！`);
+    },err=>{
+        console.log(`创建集合: ${name} 失败！`);
+        console.log(err);
     });
 };
+function insert(obj_,dbase){
+    const {obj,collectionName} = obj_;
+    if (!(typeof obj === "object" && obj)){
+        console.log('请输入object类型的集合名');
+        return;
+    }
+    dbase.collection(collectionName).insert(obj).then(result=>{
+        console.log(`集合 ${collectionName} 插入数据成功！`);
+        console.log(obj);
+    },err=>{
+        console.log(`集合 ${collectionName} 插入数据失败！`);
+        console.log(err);
+    })
+};
+
+async function find(obj_,dbase){
+    const {obj={},collectionName} = obj_;
+    if (!(typeof obj === "object" && obj)){
+        console.log('请输入object类型的集合名');
+        return;
+    }
+    let a = null;
+    return dbase.collection(collectionName).find(obj).toArray().then(result=>{
+        console.log("查询成功！");
+        result.forEach((value,index,arr)=>{
+            console.log(value);
+        })
+        return result;
+    },err=>{
+        console.log(`查询失败！`);
+        console.log(err);
+    });
+    // return a;
+};
+
+module.exports = mongoDB_Obj
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,41 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const mongoDB = require('../public/javascripts/mongoDB/connect');
+const mongoose = require('mongoose');
+
+const db = new mongoDB({
+  user:"edgejeanblog",
+  pws:"sdd19961103",
+  host:"118.25.150.243",
+  database:"edgejeanblog"
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+// router.get('/connect/addCollection/:name', (req, res) => {
+//   res.render('index', { title: req.params.name });
+//   db.collection(req.params.name);
+// })
+router.get('/connect/sel/:name', (req, res) => {
+  res.send(db.find({},req.params.name));
+})
+router.post('/connect/addBookmarks',(req,res)=>{
+  res.render('index', {title: req.params.name});
+  const {collectionName, name} = req.params;
+  db.insert({
+    name
+  }, collectionName);
+})
+router.post('/connect/addBookmarksTypes',(req,res)=>{
+  res.send();
+  const {collectionName,name,url,bookmarkid} = req.params;
+  db.insert({
+    name,
+    url,
+    bookmark_id:mongoose.Types.ObjectId(bookmarkid)
+  },collectionName);
+})
 
 module.exports = router;

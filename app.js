9db281519c9b7c60380d7b35bdd30e9943019c9c
Jason Liu <xaiweiok@vip.qq.com>
2020-11-04 18:11:07 +0800

    更新

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.use(lessMiddleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/api', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
